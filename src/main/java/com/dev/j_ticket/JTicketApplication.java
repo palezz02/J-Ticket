4403f113c8aa9107c36021e3eda7732f79a8a8ad
gabriele_palezz <g.palezza@gmail.com>
2026-01-08 14:15:35 +0100

    chore: setup project skeleton, docker-compose for postgres and gitignore

---
+++ src/main/java/com/dev/j_ticket/JTicketApplication.java
@@ -0,0 +1,13 @@
+package com.dev.j_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/j_ticket/JTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.j_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
