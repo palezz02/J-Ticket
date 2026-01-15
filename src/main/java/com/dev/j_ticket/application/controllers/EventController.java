b13a300ce800c76214c9a55fbf58f8e1ff7ca6e3
gabriele_palezz <g.palezza@gmail.com>
2026-01-15 14:23:06 +0100

    Feat: restructure packages and finalize database schema for events

---
+++ src/main/java/com/dev/j_ticket/application/controllers/EventController.java
@@ -0,0 +1,26 @@
+package com.dev.j_ticket.application.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.j_ticket.application.services.EventService;
+import com.dev.j_ticket.domain.models.Event;
+
+@RestController
+@RequestMapping("/api/v1/events")
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public List<Event> getAll() {
+        return eventService.getAllEvents();
+    }
+}
