b087a86caae59d71dfd436e2bbce78f063c5b3e9
gabriele_palezz <g.palezza@gmail.com>
2026-01-16 19:39:55 +0100

    feat: setup flyway and initial schema for tickets, events and users

--- src/main/resources/db/migration/V1__create_initial_schema.sql
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -1,13 +1,43 @@
+DROP TABLE IF EXISTS events CASCADE;
 
-CREATE TABLE events (
+CREATE TABLE IF NOT EXISTS events (
     id BIGSERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description TEXT,
-    location VARCHAR(100) NOT NULL DEFAULT 'Milano',
+    location VARCHAR(100) NOT NULL DEFAULT 'Florence',
     event_date TIMESTAMP NOT NULL,
     available_tickets INT NOT NULL,
     version INT DEFAULT 0
 );
 
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tickets (
+    id BIGSERIAL PRIMARY KEY,
+    price DECIMAL(10,2) NOT NULL,
+    serial_number VARCHAR(100) NOT NULL UNIQUE,
+    user_id BIGINT NOT NULL,
+    event_id BIGINT NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_event FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
+);
+
+
+-- Inserimento Eventi
 INSERT INTO events (title, description, location, event_date, available_tickets, version)
-VALUES ('Grand Opening Green Day', 'Evento inaugurale a Milano', 'Fiera Milano (Rho)', '2026-06-01 21:00:00', 500, 0);
+VALUES ('Grand Opening Green Day', 'Evento inaugurale a Firenze', 'Visarno Arena', '2026-06-01 21:00:00', 500, 0);
+
+-- Inserimento Utenti
+INSERT INTO users (id, username, email, password) 
+VALUES (1, 'mario_rossi', 'mario@email.it', 'password123');
+
+-- Inserimento Ticket (Collega utente 1 con l'evento dei Green Day)
+INSERT INTO tickets (price, serial_number, user_id, event_id)
+VALUES (75.50, 'TKT-GD-2026-001', 1, (SELECT id FROM events WHERE title = 'Grand Opening Green Day' LIMIT 1));
+
+
